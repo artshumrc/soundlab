9894e61ee73a6ac6466bda33c53d8b2ec83a1d84
Igor <pasieczny.igor@gmail.com>
2017-06-29 15:36:16 +0200

    add fontawesome package, add menu items

--- client/src/components/navigation/Header.js
+++ client/src/components/navigation/Header.js
@@ -1,23 +1,46 @@
 import React from 'react';
-import {Navbar} from 'react-bootstrap';
+import {Navbar, Nav, NavItem, NavDropdown, MenuItem} from 'react-bootstrap';
+import FontAwesome from 'react-fontawesome';
 import './Header.css';
 
 export default class Header extends React.Component {
-  render() {
-    return (
-      <div>
-        <Navbar className="social-nav" fixedTop={true}>
-          <Navbar.Header>
-          </Navbar.Header>
-        </Navbar>
-        <Navbar className="page-nav" fixedTop={true}>
-          <Navbar.Header>
-            <Navbar.Brand>
+	render() {
+		return (
+			<div>
+				<Navbar className="social-nav" fixedTop>
+					<Nav>
+						<NavItem eventKey={1} href="#"><FontAwesome name="facebook-official" className="social-icon" /></NavItem>
+						<NavItem eventKey={2} href="#"><FontAwesome name="twitter" className="social-icon" /></NavItem>
+						<NavItem eventKey={3} href="#"><FontAwesome name="pinterest" className="social-icon" /></NavItem>
+						<NavItem eventKey={4} href="#"><FontAwesome name="instagram" className="social-icon" /></NavItem>
+						<NavItem eventKey={5} href="#"><FontAwesome name="skype" className="social-icon" /></NavItem>
+						<NavItem eventKey={6} href="#"><FontAwesome name="dribbble" className="social-icon" /></NavItem>
+					</Nav>
+					<Nav pullRight>
+						<NavItem eventKey={1} href="#"><FontAwesome name="envelope-o" className="social-icon" /> Newsletter</NavItem>
+						<NavItem eventKey={2} href="#"><FontAwesome name="key" className="social-icon" /> Login</NavItem>
+						<NavItem eventKey={3} href="#"><FontAwesome name="unlock-alt" className="social-icon" /> Register</NavItem>
+					</Nav>
+				</Navbar>
+				<Navbar className="page-nav" fixedTop>
+					<Navbar.Header>
+						<Navbar.Brand>
               Orpheus
             </Navbar.Brand>
-          </Navbar.Header>
-        </Navbar>
-      </div>
-    );
-  }
+					</Navbar.Header>
+					<Nav>
+						<NavItem eventKey={1} href="#">Home</NavItem>
+						<NavItem eventKey={2} href="#">About</NavItem>
+						<NavItem eventKey={3} href="#">Features</NavItem>
+						<NavItem eventKey={4} href="#">Portfolio</NavItem>
+						<NavItem eventKey={5} href="#">Testimonials</NavItem>
+					</Nav>
+					<Nav pullRight>
+						<NavItem eventKey={1} href="#"><FontAwesome name="search" className="social-icon" /></NavItem>
+						<NavItem eventKey={2} href="#"><FontAwesome name="bars" className="social-icon" /></NavItem>
+					</Nav>
+				</Navbar>
+			</div>
+		);
+	}
 }
--- client/src/containers/Root.js
+++ client/src/containers/Root.js
@@ -11,7 +11,7 @@ const Root = ({store, history}) => (
 			<BrowserRouter>
 				{Routes}
 			</BrowserRouter>
-			<DevTools />
+			{/*<DevTools />*/}
 		</div>
 	</Provider>
 );
