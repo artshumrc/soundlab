42b9ecdf72469c36b12e28c3c05b8bc94b32a249
Igor <pasieczny.igor@gmail.com>
2017-06-30 10:45:48 +0200

    extract about section from Home component

--- client/src/components/home/Home/Home.js
+++ client/src/components/home/Home/Home.js
@@ -2,6 +2,7 @@ import React from 'react';
 import {Jumbotron, Button, Grid, Row, Col, Image} from 'react-bootstrap';
 import './Home.css';
 import Header from '../../navigation/Header';
+import AboutSection from './sections/AboutSection';
 
 class Home extends React.Component {
 	render() {
@@ -19,29 +20,8 @@ class Home extends React.Component {
 					</div>
 					<div className="layer" />
 				</Jumbotron>
-				<section>
-					<Grid>
-						<Row>
-							<Col lg={4}>
-								<Image src="/images/engineer.png" responsive />
-							</Col>
-							<Col lg={8}>
-								<h2><span className="highlight">Orpheus</span> is for your digital archive</h2>
-								<p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the
-                  industry`s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and
-                  scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap.
-                </p>
-								<hr />
-								<p className="italic">"Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the
-                  industry`s standard dummy text ever since the 1500s, when an unknown printer took a galley of type
-                  and scrambled it to make a type specimen book."
-                </p>
-								<hr className="short orange" />
-								<p className="signature"><strong>John Doe</strong> - CEO/Founder ZAP</p>
-							</Col>
-						</Row>
-					</Grid>
-				</section>
+        <AboutSection/>
+
 			</div>
 		);
 	}
