f61129108102717b676180c8c3f34d3331e1c008
Michał Pierzchlewicz <kontakt@michalpierzchlewicz.pl>
2017-06-30 16:24:56 +0200

    add redux devtools extension to work with chrome

--- client/src/store/configureStore.js
+++ client/src/store/configureStore.js
@@ -9,6 +9,7 @@ const configureStore = preloadedState => {
 	const store = createStore(
 		rootReducer,
 		preloadedState,
+		window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
 		compose(
 			applyMiddleware(thunk, api, createLogger()),
 			DevTools.instrument()
