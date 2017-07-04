c9b8ec1858005c771dc3080863a62908d45a54fb
Igor <pasieczny.igor@gmail.com>
2017-07-04 17:14:14 +0200

    add navitagion panel to the dashboard

--- client/src/components/dashboard/Dashboard.js
+++ client/src/components/dashboard/Dashboard.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import './Dashboard.css';
 import ListItem from './components/ListItem';
+import FontAwesome from 'react-fontawesome';
+import {Image} from 'react-bootstrap';
 
 export default class Dashboard extends React.Component {
   constructor(props) {
@@ -25,6 +27,28 @@ export default class Dashboard extends React.Component {
         </div>
         <div className="contentWrapper">
           <div className="topNav">
+            <a href="#">
+              <FontAwesome name="caret-left"/>
+              <FontAwesome name="bars" size="2x"/>
+            </a>
+            <div className="navigation pull-right">
+              <a href="#">
+                <div className="userPanel">
+                  <div className="userImage">
+                    <span className="status"></span>
+                    <Image src="/images/bw.png" circle/>
+                  </div>
+                  <div>
+                    Bruce Wayne
+                  </div>
+                  <FontAwesome name="arrow-down"/>
+                </div>
+              </a>
+              <div className="notifications">
+                <FontAwesome name="bell"/>
+                <FontAwesome name="envelope-o"/>
+              </div>
+            </div>
           </div>
           <div className="dashboardContent">
 
