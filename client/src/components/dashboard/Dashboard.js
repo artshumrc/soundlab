e8f2d8f58e55ab63cb47a747144ad7bfd0526bc0
Igor <pasieczny.igor@gmail.com>
2017-07-05 11:01:41 +0200

    add features to top navigation of the dashboard

--- client/src/components/dashboard/Dashboard.js
+++ client/src/components/dashboard/Dashboard.js
@@ -1,60 +1,86 @@
 import React from 'react';
-import './Dashboard.css';
-import ListItem from './components/ListItem';
 import FontAwesome from 'react-fontawesome';
 import {Image} from 'react-bootstrap';
+import './Dashboard.css';
+import ListItem from './components/ListItem';
 
 export default class Dashboard extends React.Component {
-  constructor(props) {
-    super(props);
-  }
+	constructor(props) {
+		super(props);
+		this.toggleSidebar = this.toggleSidebar.bind(this);
+	}
 
-  render() {
-    return (
-      <div id="dashboard">
-        <div className="sidebar">
-          <h3 className="invert">Orpheus</h3>
-          <div className="sidebarMenu">
-            <ul>
-              <ListItem fa="file-text-o" name="Main Panel" active/>
-              <ListItem fa="file" name="Articles"/>
-              <ListItem fa="user" name="User Profile"/>
-              <ListItem fa="window-maximize" name="Data Entry"/>
-              <ListItem fa="folder-open-o" name="Projects"/>
-              <ListItem fa="cog" name="Settings"/>
-            </ul>
-          </div>
-        </div>
-        <div className="contentWrapper">
-          <div className="topNav">
-            <a href="#">
-              <FontAwesome name="caret-left"/>
-              <FontAwesome name="bars" size="2x"/>
-            </a>
-            <div className="navigation pull-right">
-              <a href="#">
-                <div className="userPanel">
-                  <div className="userImage">
-                    <span className="status"></span>
-                    <Image src="/images/bw.png" circle/>
-                  </div>
-                  <div>
+	toggleSidebar() {
+		if (this.sidebar.classList.contains('sidebarWrapped')) {
+			this.contentWrapper.classList.remove('contentWrapperExpand');
+			this.sidebar.classList.remove('sidebarWrapped');
+		} else {
+			this.contentWrapper.classList.add('contentWrapperExpand');
+			this.sidebar.classList.add('sidebarWrapped');
+		}
+	}
+
+	render() {
+		return (
+			<div id="dashboard">
+				<div
+					className="sidebar"
+					ref={(div) => {
+						this.sidebar = div;
+					}}
+				>
+					<h3 className="invert">Orpheus</h3>
+					<div className="sidebarMenu">
+						<ul>
+							<ListItem fa="file-text-o" name="Main Panel" active />
+							<ListItem fa="file" name="Articles" />
+							<ListItem fa="user" name="User Profile" />
+							<ListItem fa="window-maximize" name="Data Entry" />
+							<ListItem fa="folder-open-o" name="Projects" />
+							<ListItem fa="cog" name="Settings" />
+						</ul>
+					</div>
+				</div>
+				<div
+					className="contentWrapper"
+					ref={(div) => {
+						this.contentWrapper = div;
+					}}
+				>
+					<div className="topNav">
+						<a href="#" onClick={this.toggleSidebar} className="toggleSidebar">
+							<FontAwesome name="caret-left" />
+							<FontAwesome name="bars" size="2x" />
+						</a>
+						<input type="text" className="searchInput" placeholder="Search..." />
+						<div className="navigation pull-right">
+							<a href="#">
+								<div className="userPanel">
+									<div className="userImage">
+										<span className="status" />
+										<Image src="/images/bw.png" circle />
+									</div>
+									<div>
                     Bruce Wayne
                   </div>
-                  <FontAwesome name="arrow-down"/>
-                </div>
-              </a>
-              <div className="notifications">
-                <FontAwesome name="bell"/>
-                <FontAwesome name="envelope-o"/>
-              </div>
-            </div>
-          </div>
-          <div className="dashboardContent">
-
-          </div>
-        </div>
-      </div>
-    )
-  }
+									<FontAwesome name="arrow-down" />
+								</div>
+							</a>
+							<div className="notifications">
+								<div className="notification">
+									<FontAwesome name="bell" />
+									<span className="notificationBadge">13</span>
+								</div>
+								<div className="notification">
+									<span className="notificationBadge">1</span>
+									<FontAwesome name="envelope-o" />
+								</div>
+							</div>
+						</div>
+					</div>
+					<div className="dashboardContent" />
+				</div>
+			</div>
+		);
+	}
 }
--- client/src/components/dashboard/components/ListItem.js
+++ client/src/components/dashboard/components/ListItem.js
@@ -2,18 +2,14 @@ import React from 'react';
 import FontAwesome from 'react-fontawesome';
 
 export default class ListItem extends React.Component {
-  constructor(props) {
-    super(props);
-  }
-
-  render() {
-    const active = this.props.active ? 'active' : '';
-    return (
-      <a href="#">
-        <li className={active}>
-          <FontAwesome name={this.props.fa}/> {this.props.name}
-        </li>
-      </a>
-    );
-  }
+	render() {
+		const active = this.props.active ? 'active' : '';
+		return (
+			<a href="#">
+				<li className={active}>
+					<FontAwesome name={this.props.fa} /> <span className="listItemText">{this.props.name}</span>
+				</li>
+			</a>
+		);
+	}
 }
