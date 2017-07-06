4339c704ca92e9e0f9132c52e043970071429620
Igor <pasieczny.igor@gmail.com>
2017-07-06 10:22:42 +0200

    fix urls in dashboard

--- client/src/components/dashboard/Dashboard.js
+++ client/src/components/dashboard/Dashboard.js
@@ -7,74 +7,74 @@ import Articles from './routes/Articles';
 import ListItem from './components/ListItem';
 
 export default class Dashboard extends React.Component {
-  constructor(props) {
-    super(props);
-    this.toggleSidebar = this.toggleSidebar.bind(this);
-    this.state = {
-      sidebarVisible: true
-    };
-  }
+	constructor(props) {
+		super(props);
+		this.toggleSidebar = this.toggleSidebar.bind(this);
+		this.state = {
+			sidebarVisible: true
+		};
+	}
 
-  toggleSidebar() {
-    this.setState({sidebarVisible: !this.state.sidebarVisible})
-  }
+	toggleSidebar() {
+		this.setState({sidebarVisible: !this.state.sidebarVisible});
+	}
 
-  render() {
-    const sidebarClass = this.state.sidebarVisible ? 'sidebar' : 'sidebar sidebarWrapped';
-    const contentWrapperClass = this.state.sidebarVisible ? 'contentWrapper' : 'contentWrapper contentWrapperExpand';
+	render() {
+		const sidebarClass = this.state.sidebarVisible ? 'sidebar' : 'sidebar sidebarWrapped';
+		const contentWrapperClass = this.state.sidebarVisible ? 'contentWrapper' : 'contentWrapper contentWrapperExpand';
 
-    return (
-      <div id="dashboard">
-        <div className={sidebarClass}>
-          <h3 className="invert">Orpheus</h3>
-          <div className="sidebarMenu">
-            <ul>
-              <ListItem fa="file-text-o" name="Main Panel" active/>
-              <ListItem fa="file" name="Articles"/>
-              <ListItem fa="user" name="User Profile"/>
-              <ListItem fa="window-maximize" name="Data Entry"/>
-              <ListItem fa="folder-open-o" name="Projects"/>
-              <ListItem fa="cog" name="Settings"/>
-            </ul>
-          </div>
-        </div>
-        <div className={contentWrapperClass}>
-          <div className="topNav">
-            <a href="#" onClick={this.toggleSidebar} className="toggleSidebar">
-              <FontAwesome name="caret-left"/>
-              <FontAwesome name="bars" size="2x"/>
-            </a>
-            <input type="text" className="searchInput" placeholder="Search..."/>
-            <div className="navigation pull-right">
-              <a href="#">
-                <div className="userPanel">
-                  <div className="userImage">
-                    <span className="status"/>
-                    <Image src="/images/bw.png" circle/>
-                  </div>
-                  <div>
+		return (
+			<div id="dashboard">
+				<div className={sidebarClass}>
+					<h3 className="invert">Orpheus</h3>
+					<div className="sidebarMenu">
+						<ul>
+							<ListItem fa="file-text-o" name="Main Panel" url="/dashboard" />
+							<ListItem fa="file" name="Articles" url="/dashboard/articles" />
+							<ListItem fa="user" name="User Profile" url="/dashboard/user" />
+							<ListItem fa="window-maximize" name="Data Entry" url="/dashboard/data" />
+							<ListItem fa="folder-open-o" name="Projects" url="/dashboard/projects" />
+							<ListItem fa="cog" name="Settings" url="/dashboard/settings" />
+						</ul>
+					</div>
+				</div>
+				<div className={contentWrapperClass}>
+					<div className="topNav">
+						<a href="#" onClick={this.toggleSidebar} className="toggleSidebar">
+							<FontAwesome name="caret-left" />
+							<FontAwesome name="bars" size="2x" />
+						</a>
+						<input type="text" className="searchInput" placeholder="Search..." />
+						<div className="navigation pull-right">
+							<a href="#">
+								<div className="userPanel">
+									<div className="userImage">
+										<span className="status" />
+										<Image src="/images/bw.png" circle />
+									</div>
+									<div>
                     Bruce Wayne
                   </div>
-                  <FontAwesome name="arrow-down"/>
-                </div>
-              </a>
-              <div className="notifications">
-                <div className="notification">
-                  <FontAwesome name="bell"/>
-                  <span className="notificationBadge">13</span>
-                </div>
-                <div className="notification">
-                  <span className="notificationBadge">1</span>
-                  <FontAwesome name="envelope-o"/>
-                </div>
-              </div>
-            </div>
-          </div>
-          <div className="dashboardContent">
-            <Route path="/dashboard/articles" component={Articles}/>
-          </div>
-        </div>
-      </div>
-    );
-  }
+									<FontAwesome name="arrow-down" />
+								</div>
+							</a>
+							<div className="notifications">
+								<div className="notification">
+									<FontAwesome name="bell" />
+									<span className="notificationBadge">13</span>
+								</div>
+								<div className="notification">
+									<span className="notificationBadge">1</span>
+									<FontAwesome name="envelope-o" />
+								</div>
+							</div>
+						</div>
+					</div>
+					<div className="dashboardContent">
+						<Route path="/dashboard/articles" component={Articles} />
+					</div>
+				</div>
+			</div>
+		);
+	}
 }
--- client/src/components/dashboard/components/ListItem.js
+++ client/src/components/dashboard/components/ListItem.js
@@ -5,7 +5,7 @@ export default class ListItem extends React.Component {
 	render() {
 		const active = this.props.active ? 'active' : '';
 		return (
-			<a href="#">
+			<a href={this.props.url}>
 				<li className={active}>
 					<FontAwesome name={this.props.fa} /> <span className="listItemText">{this.props.name}</span>
 				</li>
