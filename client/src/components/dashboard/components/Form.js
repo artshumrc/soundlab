35fbeee36ac859cec3d91affd1a96e25a7b6e0f5
Igor <pasieczny.igor@gmail.com>
2017-07-21 09:24:51 +0200

    add submit button style

--- client/src/components/dashboard/components/Form.js
+++ client/src/components/dashboard/components/Form.js
@@ -1,17 +1,16 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import {reduxForm} from 'redux-form';
+import './stylesheets/Form.css';
 
 class Form extends React.Component {
-	constructor(props) {
-		super(props);
-	}
-
 	render() {
 		return (
 			<form onSubmit={this.props.handleSubmit}>
 				{this.props.children}
-				<button type="submit">Submit</button>
+				<div className="submitButton">
+					<button type="submit">Submit</button>
+				</div>
 			</form>
 		);
 	}
