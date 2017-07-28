fd0b82c5ccb88ef77b25590c93252a5060fca9c0
Michał Pierzchlewicz <kontakt@michalpierzchlewicz.pl>
2017-07-28 11:32:53 +0200

    tenant api tests

--- server/api/tenant.test.js
+++ server/api/tenant.test.js
@@ -2,15 +2,21 @@ import faker from 'faker';
 import mockingoose from 'mockingoose';
 import mongoose from 'mongoose';
 
-// model
-import TenantModel from '../models/tenant';
-
 // tested module
 import TenantClass from './tenant';
 
 describe('TenantClass', () => {
 
+	const projectId = mongoose.Types.ObjectId();
+	const name = faker.commerce.productName();
+
 	describe('create', () => {
-		test();
+		test('ModelAPI create method is called wth correct params', async () => {
+			const Tenant = new TenantClass();
+			const ModelAPI = Object.getPrototypeOf(Object.getPrototypeOf(Tenant));
+			ModelAPI.create = jest.fn();
+			await Tenant.create(projectId, name);
+			expect(ModelAPI.create).toHaveBeenCalledWith(projectId, { name }, process.env.DEFAULT_LANGUAGE);
+		});
 	});
 });
