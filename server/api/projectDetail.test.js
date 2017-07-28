c97c948632e5e553990d301849604e758d8c5fa0
Michał Pierzchlewicz <kontakt@michalpierzchlewicz.pl>
2017-07-28 11:45:23 +0200

    projectDetail api tests

--- server/api/projectDetail.test.js
+++ server/api/projectDetail.test.js
@@ -2,15 +2,21 @@ import faker from 'faker';
 import mockingoose from 'mockingoose';
 import mongoose from 'mongoose';
 
-// model
-import ProjectDetailModel from '../models/projectDetail';
-
 // tested module
-import ProjectDetailClass, { getProjectDetails } from './projectDetail';
+import ProjectDetailClass from './projectDetail';
 
 describe('ProjectDetailClass', () => {
 
+	const projectId = mongoose.Types.ObjectId();
+	const title = faker.commerce.productName();
+
 	describe('create', () => {
-		test();
+		test('ModelAPI create method is called wth correct params', async () => {
+			const ProejctDeatil = new ProjectDetailClass();
+			const ModelAPI = Object.getPrototypeOf(Object.getPrototypeOf(ProejctDeatil));
+			ModelAPI.create = jest.fn();
+			await ProejctDeatil.create(projectId, title);
+			expect(ModelAPI.create).toHaveBeenCalledWith(projectId, { title }, process.env.DEFAULT_LANGUAGE);
+		});
 	});
 });
