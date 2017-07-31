b4954da14327394e14edf1f1263ebf565fc940cf
Michał Pierzchlewicz <kontakt@michalpierzchlewicz.pl>
2017-07-31 18:37:06 +0200

    filed model vlaidation

--- server/models/field.js
+++ server/models/field.js
@@ -1,48 +1,121 @@
 import mongoose from 'mongoose';
+import check from 'check-types';
 
 // plug-ins
 import timestamp from 'mongoose-timestamp';
-import language from './plugins/language';
 
 const Schema = mongoose.Schema;
 
 /**
- * CollectionDetail base schema
+ * Filed base schema
  * @type {Schema}
  */
-const CollectionDetailSchema = new Schema({
-	title: {
-		type: String,
-		unique: true,
+const FieldSchema = new Schema({
+	itemSchemaId: {
+		type: Schema.Types.ObjectId,
+		ref: 'ItemSchema',
 		required: true,
-		trim: true,
 		index: true
 	},
-	collectionId: {
-		type: Schema.Types.ObjectId,
-		ref: 'Collection',
-		index: true,
+	type: {
+		type: Object,
 		required: true,
+		enum: ['String', 'Number', 'Date', 'Boolean', 'ObjectId'],
 	},
-	description: String,
+	required: {
+		type: Boolean,
+		default: false,
+	},
+	default: {
+		type: Schema.Types.Mixed,
+	},
+	ref: {
+		type: String,
+	},
+	enum: {
+		type: Array,
+	},
+	min: {
+		type: Number,
+	},
+	max: {
+		type: Number,
+	},
 });
 
 
 // add timestamps (createdAt, updatedAt)
-CollectionDetailSchema.plugin(timestamp);
+FieldSchema.plugin(timestamp);
 
-// add slug (slug)
-CollectionDetailSchema.plugin(URLSlugs('title'));
-
-// add language (language)
-CollectionDetailSchema.plugin(language);
+// Custom validation rules
+FieldSchema.pre('validate', (next) => {
+	switch(this.type) {
+	case 'String': {
+		if (!check.string(this.default)) next(Error(`filed 'default' is type of ${typeof this.default}. Must be a string`));
+		if (this.ref) next(Error(`filed 'ref' can not be set with type = string`));
+		if (this.enum instanceof Array) {
+			this.enum.forEach((enumValue) => {
+				if (!check.string(enumValue)) {
+					next(Error(`filed 'enum' is type of ${typeof enumValue}. Must be a string`));
+				}
+			});
+		}
+		if (this.min) next(Error(`filed 'min' can not be set with type = string`));
+		if (this.max) next(Error(`filed 'max' can not be set with type = string`));
+		break;
+	}
+	case 'Number': {
+		if (!check.number(this.default)) next(Error(`filed 'default' is type of ${typeof this.default}. Must be a number`));
+		if (this.ref) next(Error(`filed 'ref' can not be set with type = number`));
+		if (this.enum instanceof Array) {
+			this.enum.forEach((enumValue) => {
+				if (!check.number(enumValue)) {
+					next(Error(`filed 'enum' is type of ${typeof enumValue}. Must be a number`));
+				}
+			});
+		}
+		if (!check.number(this.min)) next(Error(`filed 'min' is type of ${typeof this.default}. Must be a number`));
+		if (!check.number(this.max)) next(Error(`filed 'max' is type of ${typeof this.default}. Must be a number`));
+		break;
+	}
+	case 'Date': {
+		if (!check.date(this.default)) next(Error(`filed 'default' is type of ${typeof this.default}. Must be a date`));
+		if (this.ref) next(Error(`filed 'ref' can not be set with type = date`));
+		if (this.enum instanceof Array) {
+			this.enum.forEach((enumValue) => {
+				if (!check.date(enumValue)) {
+					next(Error(`filed 'enum' is type of ${typeof enumValue}. Must be a date`));
+				}
+			});
+		}
+		if (!check.date(this.min)) next(Error(`filed 'min' is type of ${typeof this.default}. Must be a date`));
+		if (!check.date(this.max)) next(Error(`filed 'max' is type of ${typeof this.default}. Must be a date`));
+		break;
+	}
+	case 'Boolean': {
+		if (!check.boolean(this.default)) next(Error(`filed 'default' is type of ${typeof this.default}. Must be a boolean`));
+		if (this.ref) next(Error(`filed 'ref' can not be set with type = boolean`));
+		if (this.enum) next(Error(`filed 'enum' can not be set with type = boolean`));
+		if (this.min) next(Error(`filed 'min' can not be set with type = boolean`));
+		if (this.max) next(Error(`filed 'max' can not be set with type = boolean`));
+		break;
+	}
+	case 'ObjectId': {
+		if (this.default instanceof Schema.Types.ObjectId) next(Error(`filed 'default' is not instance of ObjectId`));
+		if (this.enum) next(Error(`filed 'enum' can not be set with type = ObjectId`));
+		if (this.min) next(Error(`filed 'min' can not be set with type = ObjectId`));
+		if (this.max) next(Error(`filed 'max' can not be set with type = ObjectId`));
+		break;
+	}
+	}
+});
 
 /**
- * CollectionDetail mongoose model
+ * Filed mongoose model
  * @type {Object}
  */
-const CollectionDetail = mongoose.model('CollectionDetail', CollectionDetailSchema);
+const Filed = mongoose.model('Filed', FieldSchema);
 
-export default CollectionDetail;
-export { CollectionDetailSchema };
+export default Filed;
+export { FieldSchema };
 
