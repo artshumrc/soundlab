d0a9746c6697a07e63e264a49390326220787a4b
Luke Hollis <lukehollis@gmail.com>
2017-08-06 08:49:33 -0400

    add potential for specifying db user and pass in env for mongoose

--- server/mongoose.js
+++ server/mongoose.js
@@ -11,6 +11,12 @@ const getURL = () => {
 	const DB_HOST = process.env.DB_HOST;
 	const DB_PORT = process.env.DB_PORT;
 	const DB_NAME = process.env.DB_NAME;
+	const DB_USER = process.env.DB_USER;
+	const DB_PASS = process.env.DB_PASS;
+
+	if (DB_USER && DB_PASS) {
+		return `mongodb://${DB_USER}:${DB_PASS}@${DB_HOST}:${DB_PORT}/${DB_NAME}`;
+	}
 
 	return `mongodb://${DB_HOST}:${DB_PORT}/${DB_NAME}`;
 };
