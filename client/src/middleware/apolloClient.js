fa0b5e1f320e44c523bfc27ea1a475c54ee33fdd
Tyler Kirby <joseph.kirby12@ncf.edu>
2017-08-29 10:14:52 -0500

    ws client removed

--- client/src/middleware/apolloClient.js
+++ client/src/middleware/apolloClient.js
@@ -23,19 +23,19 @@ const connectionParams = () => {
 	return { authToken: localStorage.getItem('token') ? localStorage.getItem('token') : null };
 };
 
-const wsClient = new SubscriptionClient(`${process.env.REACT_APP_WS_SERVER}/${process.env.REACT_APP_WS_SERVER_URI}`, {
-	reconnect: true,
-	connectionParams,
-});
+// const wsClient = new SubscriptionClient(`${process.env.REACT_APP_WS_SERVER}/${process.env.REACT_APP_WS_SERVER_URI}`, {
+// 	reconnect: true,
+// 	connectionParams,
+// });
 
-const networkInterfaceWithSubscriptions = addGraphQLSubscriptions(
-	networkInterface,
-	wsClient
-);
+// const networkInterfaceWithSubscriptions = addGraphQLSubscriptions(
+// 	networkInterface,
+// 	wsClient
+// );
 
 const client = new ApolloClient({
-	networkInterface: networkInterfaceWithSubscriptions,
+	networkInterface
 });
 
 export default client;
-export { wsClient };
+// export { wsClient };
