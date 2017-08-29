bfbc48317c4e364580e997df2ff88b1d008cab2b
Tyler Kirby <joseph.kirby12@ncf.edu>
2017-08-29 12:29:16 -0500

    added prop validation to button

--- client/src/components/common/buttons/Button/Button.js
+++ client/src/components/common/buttons/Button/Button.js
@@ -1,34 +1,46 @@
 import React from 'react';
+import { Link } from 'react-router';
 import './Button.css';
 
-const Button = props => {
+const Button = (props) => {
 
-	const { href, onClick, light, transparentLight, primary, accent, outline } = props;
+	const { to, onClick, light, transparentLight, primary, accent, outline } = props;
 	const classes = ['orpheus-button'];
 
 	if (primary) {
-		classes.push('orpheus-button--primary')
+		classes.push('orpheus-button--primary');
 	} else if (accent) {
-		classes.push('orpheus-button--accent')
+		classes.push('orpheus-button--accent');
 	} else if (light) {
-		classes.push('orpheus-button--light')
+		classes.push('orpheus-button--light');
 	} else if (transparentLight) {
-		classes.push('orpheus-button--trans-light')
+		classes.push('orpheus-button--trans-light');
 	}
 
 	if (outline) {
-		classes.push('orpheus-button--outline')
+		classes.push('orpheus-button--outline');
 	}
 
 	return (
-		<a
-			href={href}
+		<Link
+			to={to}
 			onClick={onClick}
 			className={classes.join(' ')}
 		>
 			{props.children}
-		</a>
-	)
-}
+		</Link>
+	);
+};
+
+Button.propTypes = {
+	to: PropTypes.string.isRequired,
+	onClick: PropTypes.func.isRequired,
+	light: PropTypes.string,
+	transparentLight: PropTypes.string,
+	primary: PropTypes.string,
+	accent: PropTypes.string,
+	outline: PropTypes.string,
+	children: PropTypes.node.isRequired
+};
 
 export default Button;
