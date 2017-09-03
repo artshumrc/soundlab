858d4b095a8df1ee818b7db8ea82950e97460d63
Tyler Kirby <tyler.kirby9398@gmail.com>
2017-09-03 08:21:42 -0500

    refactored projectEditor, redux form submit works now

--- client/src/components/dashboard/routes/projectEditor/ProjectEditor.js
+++ client/src/components/dashboard/routes/projectEditor/ProjectEditor.js
@@ -4,6 +4,7 @@ import { Field, FieldArray, SubmissionError, reduxForm } from 'redux-form';
 import Textarea from 'react-textarea-autosize';
 import { gql, graphql } from 'react-apollo';
 import { connect } from 'react-redux';
+import { compose } from 'redux';
 
 import Form from '../../components/Form';
 import TagEditor from '../itemEditor/TagEditor';
@@ -12,67 +13,41 @@ import MetaEditor from '../itemEditor/MetaEditor';
 // TODO: Fix post method
 // TODO: Remove autofocus for accessibility reasons
 
-
-class ProjectEditor extends React.Component {
-	constructor(props) {
-		super(props);
-
-		this.handleSubmit = this.handleSubmit.bind(this);
-	}
-
-	async handleSubmit(values, dispatch) {
-		const { createNewProject } = this.props;
-		try {
-			console.log(values);
-			await createNewProject(values);
-			return {};
-		} catch (err) {
-			throw new SubmissionError({ _error: 'your error' });
-		}
-	}
-
-	render() {
-		return (
-			<div className="content">
-				<div className="itemEditor">
-					<Form
-						onSubmit={this.handleSubmit}
-						form="projectEditor"
-						initialValues={this.state}
-					>
-						<Field
-							name="title"
-							component="input"
-							type="text"
-							placeholder="Title..."
-							className="itemTitleEdit detailInput"
-						/>
-						<Field
-							name="description"
-							className="itemDescriptionEdit detailInput"
-							placeholder="Description..."
-							component="input"
-						/>
-						<FieldArray
-							name="tags"
-							component={tags => (
-								<TagEditor tags={tags} />
-							)}
-						/>
-					</Form>
-				</div>
-			</div>
-		);
-	}
-}
+const ProjectEditor = ({ submit }) => (
+	<div className="content">
+		<div className="itemEditor">
+			<Form
+				onSubmit={() => submit}
+				form="projectEditor"
+				initialValues={this.state}
+			>
+				<Field
+					name="title"
+					component="input"
+					type="text"
+					placeholder="Title..."
+					className="itemTitleEdit detailInput"
+				/>
+				<Field
+					name="description"
+					className="itemDescriptionEdit detailInput"
+					placeholder="Description..."
+					component="input"
+				/>
+				<FieldArray
+					name="tags"
+					component={tags => (
+						<TagEditor tags={tags} />
+					)}
+				/>
+			</Form>
+		</div>
+	</div>
+);
 
 ProjectEditor.propTypes = {
-	createNewProject: PropTypes.func.isRequired
+	submit: PropTypes.func.isRequired
 };
-
-ProjectEditor = reduxForm({
-	form: 'projectsEditor'
-})(ProjectEditor);
 
 const addNewProject = gql`
 mutation projectCreate($project: ProjectCreateInputType!) {
@@ -82,8 +57,11 @@ mutation projectCreate($project: ProjectCreateInputType!) {
 }
 `;
 
-export default graphql(addNewProject, {
-	props: ({ mutate }) => ({
-		createNewProject: project => mutate({ variables: project }),
+export default compose(
+	reduxForm({ form: 'projectsEditor' }),
+	graphql(addNewProject, {
+		props: ({ mutate }) => ({
+			submit: project => mutate({ variables: project }),
+		})
 	})
-})(ProjectEditor);
+)(ProjectEditor);
