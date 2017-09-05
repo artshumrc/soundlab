f193af121fd6f8976618a28565cadb378cddebd9
Tyler Kirby <tyler.kirby9398@gmail.com>
2017-09-04 19:37:32 -0500

    added project component

--- client/src/components/dashboard/routes/projectsView/project/Project.js
+++ client/src/components/dashboard/routes/projectsView/project/Project.js
@@ -3,13 +3,22 @@ import PropTypes from 'prop-types';
 
 const Project = ({ title, description, createdAt }) => (
 	<div>
+		<div>
+			<h2>{{ title }}</h2>
+		</div>
+		<div>
+			<h3>{{ description }}</h3>
+		</div>
+		<div>
+			<h4>{{ createdAt }}</h4>
+		</div>
 	</div>
 );
 
 Project.propTypes = {
 	title: PropTypes.string.isRequired,
 	description: PropTypes.string,
-	createdAt: PropTypes.date.isRequired
+	createdAt: PropTypes.instanceOf(Date).isRequired
 };
 
 Project.defaultProps = {
