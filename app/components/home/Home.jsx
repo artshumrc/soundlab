d5c78983a15c5cb8e80f13868042caebe1d83d64
pickering.mp <pickering.mp@gmail.com>
2017-09-05 09:35:50 -0400

    cover layout updates

--- app/components/home/Home.jsx
+++ app/components/home/Home.jsx
@@ -21,8 +21,8 @@ class Home extends React.Component {
     return (
       <MuiThemeProvider>
 
-        <div>
-          <svg width="1600px" height="1400px" styleName="cover-lines">
+
+        {/*  <svg width="1600px" height="1400px" styleName="cover-lines">
             <path d="M0,56 C400,300 500,895 1494,950" stroke="#fadbd2" fill="transparent"/>
             <path d="M0,86 C440,340 500,895 1494,960" stroke="#fadbd2" fill="transparent"/>
             <path d="M0,116 C480,380 500,895 1494,970" stroke="#fadbd2" fill="transparent"/>
@@ -39,17 +39,6 @@ class Home extends React.Component {
             <path d="M0,436 C920,850 500,895 1494,1080" stroke="#fadbd2" fill="transparent"/>
             <path d="M0,466 C960,920 500,895 1494,1080" stroke="#fadbd2" fill="transparent"/>
 
-          {/*  <path d="M0,440 C640,800 1400,1250 1694,960" stroke="orange" fill="transparent"/>
-              <path d="M0,412 C600,750 1250,1250 1694,940" stroke="purple" fill="transparent"/>
-            <path d="M0,412 C560,700 1100,1250 1694,960" stroke="green" fill="transparent"/>
-            <path d="M0,412 C520,650 950,1250 1694,980" stroke="blue" fill="transparent"/>
-            <path d="M0,412 C480,600 800,1250 1694,1000" stroke="black" fill="transparent"/>
-            <path d="M0,412 C440,550 650,1250 1654,1020" stroke="cyan" fill="transparent"/>
-            <path d="M0,412 C400,500 500,1250 1654,960" stroke="red" fill="transparent"/> */}
-
-
-
-
             <path d="M0,440 C440,650 -150,1730 1494,820" stroke="#fadbd2" fill="transparent"/>
             <path d="M0,440 C440,650 -100,1670 1494,840" stroke="#fadbd2" fill="transparent"/>
             <path d="M0,440 C440,650 -50,1610 1494,860" stroke="#fadbd2" fill="transparent"/>
@@ -65,39 +54,39 @@ class Home extends React.Component {
             <path d="M0,460 C440,650 400,1070 1494,1040" stroke="#fadbd2" fill="transparent"/>
             <path d="M0,464 C440,650 450,1010 1494,1060" stroke="#fadbd2" fill="transparent"/>
             <path d="M0,468 C440,650 500,950 1494,1080" stroke="#fadbd2" fill="transparent"/>
+          </svg> */}
+
+          <Row styleName="cover-section">
+            <Col xs={12} sm={6} md={6} lg={6}>
+              <div styleName="site-tag-container">
+                <div styleName="tag-container">
+                  <span styleName="site-tag">We hear you</span>
+                </div>
+                <div styleName="subtitle-container">
+                  <span styleName="site-tag-subtitle">The Sound Lab at Harvard University</span>
+                </div>
+              </div>
+            </Col>
+            <Col xs={12} sm={6} md={6} lg={6}>
+              <div styleName="featured-track-component" xs={12} sm={6} md={6} lg={6}>
+                <div styleName="featured-track-player-container">
+
+                </div>
+                <div styleName="featured-track-meta-container">
+                  <span styleName="featured-track-title">
+                    <h6>Featured Track</h6>
+                  </span>
+                  <span styleName="featured-track-meta-item-title">Mozart / Funk</span>
+                  <span styleName="featured-track-meta-item-author">Dollie Keller</span>
+                </div>
+              </div>
+            </Col>
+          </Row>
 
 
 
-          {/*  <path d="M0,440 C440,550 70,1250 1494,960" stroke="#fadbd2" fill="transparent"/>
-            <path d="M0,444 C440,550 150,1250 1494,980" stroke="#fadbd2" fill="transparent"/>
-            <path d="M0,448 C440,550 230,1250 1494,1000" stroke="#fadbd2" fill="transparent"/>
-            <path d="M0,452 C440,550 310,1250 1494,1020" stroke="#fadbd2" fill="transparent"/>
-            <path d="M0,456 C440,550 390,1250 1494,1040" stroke="#fadbd2" fill="transparent"/>
-            <path d="M0,460 C440,550 470,1250 1494,1060" stroke="#fadbd2" fill="transparent"/>
-            <path d="M0,464 C440,550 550,1250 1494,1080" stroke="#fadbd2" fill="transparent"/> */}
 
 
-
-
-
-
-          </svg>
-
-
-          <div styleName="site-tag-container">
-            <div styleName="tag-container">
-              <span styleName="site-tag">We hear you</span>
-            </div>
-            <div styleName="subtitle-container">
-              <span styleName="site-tag-subtitle">The Sound Lab at Harvard University</span>
-            </div>
-          </div>
-
-          <div>
-            <AudioPlayer />
-          </div>
-
-        </div>
 
       </MuiThemeProvider>
 
