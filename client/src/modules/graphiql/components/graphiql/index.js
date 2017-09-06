38fbe6800aec4c98ff3fdf534ca68ef7e8041056
Luke Hollis <lukehollis@gmail.com>
2017-09-05 21:51:41 -0400

    fix variables imports in scss files

--- client/src/modules/graphiql/components/graphiql/index.js
+++ client/src/modules/graphiql/components/graphiql/index.js
@@ -1,17 +1,20 @@
 import React from 'react';
 import GraphiQL from 'graphiql';
-import cookie from 'react-cookie';
+import Cookies from 'universal-cookie';
+
 import 'graphiql/graphiql.css';
 import './graphiql.css';
 
 import { login } from '../../../../lib/auth';
+
+const cookies = new Cookies();
 
 function graphQLFetcher(graphQLParams) {
 	return fetch(`${process.env.REACT_APP_GRAPHQL_SERVER}/${process.env.REACT_APP_GRAPHQL_URI}`, {
 		method: 'post',
 		headers: {
 			'Content-Type': 'application/json',
-			authorization: cookie.get('token') ? cookie.get('token') : null,
+			authorization: cookies.get('token') ? cookies.get('token') : null,
 		},
 		body: JSON.stringify(graphQLParams),
 	}).then(response => response.json());
