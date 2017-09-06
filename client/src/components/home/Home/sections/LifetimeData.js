e858876fac8a484909913acfb8b44ef0a09d1004
Luke Hollis <lukehollis@gmail.com>
2017-09-06 01:48:02 -0400

    add styles and interactives for lifetime data component

--- client/src/components/home/Home/sections/LifetimeData.js
+++ client/src/components/home/Home/sections/LifetimeData.js
@@ -1,44 +1,78 @@
 import React from 'react';
+import {Row, Col} from 'react-bootstrap';
+
 import './LifetimeData.css';
 
-const LifetimeData = props => {
-	const lifetimeDataMarkers = [{
-		label: 'Entry',
-		description: 'Begin by entering your data in a digital form using our sophisticated but simple data entry forms',
-	}, {
-		label: 'Publishing',
-		description: 'Publish as much of your collections as you want and embed item records in a beautifully designed content authoring interface',
-	}, {
-		label: 'Export',
-		description: 'Begin by entering your data in a digital form using our sophisticated but simple data entry forms',
-	}];
+class LifetimeData extends React.Component {
+	constructor(props) {
+		super(props);
 
-	return (
-		<div className="lifetimeData">
-			<h2>Plan for the full lifetime of your data</h2>
+		this.state = {
+			activeStep: 0,
+		};
 
-			<div className="lifetimeDataSteps">
-				{lifetimeDataMarkers.map(marker => {
-					const active = false;
+		this.handleStepHover = this.handleStepHover.bind(this);
+	}
 
-					return (
-						<div
-							key={marker.label}
-							className="lifetimeDataStep"
-						>
-							<div className="lifetimeDataMarker" />
-							<span className="lifetimeDataLabel">
-								{marker.label}
-							</span>
-							<p>
-								{marker.description}
-							</p>
-						</div>
-					);
-				})}
+	handleStepHover(newActiveStepIndex) {
+		this.setState({
+			activeStep: newActiveStepIndex,
+		});
+	}
+
+	render() {
+		const { activeStep } = this.state;
+
+		const lifetimeDataMarkers = [{
+			label: 'Entry',
+			description: 'Begin by entering your data in a digital form using our sophisticated but simple data entry forms',
+		}, {
+			label: 'Publishing',
+			description: 'Publish as much of your collections as you want and embed item records in a beautifully designed content authoring interface',
+		}, {
+			label: 'Export',
+			description: 'Begin by entering your data in a digital form using our sophisticated but simple data entry forms',
+		}];
+
+		return (
+			<div className="lifetimeData">
+				<div className="lifetimeDataVisualization">
+					<div className="lifetimeDataLineGraph" />
+				</div>
+				<div className="lifetimeDataText">
+					<h2>Plan for the full lifetime of your data</h2>
+
+					<div className="lifetimeDataSteps">
+						<Row>
+							{lifetimeDataMarkers.map((marker, i) => {
+								const active = false;
+
+								return (
+									<Col
+										lg={4}
+										key={marker.label}
+									>
+										<div
+											className={`lifetimeDataStep ${activeStep === i ? 'lifetimeDataStepActive' : ''}`}
+											onMouseOver={this.handleStepHover.bind(this, i)}
+										>
+											<div className="lifetimeDataMarker" />
+											<span className="lifetimeDataLabel">
+												{marker.label}
+											</span>
+											<p>
+												{marker.description}
+											</p>
+										</div>
+									</Col>
+								);
+							})}
+						</Row>
+					</div>
+				</div>
 			</div>
-		</div>
-	);
-};
+		);
+	}
+}
 
 export default LifetimeData;
