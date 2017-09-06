4577c3daee6852c31ea1062a01d5d8b5d2fb4094
Luke Hollis <lukehollis@gmail.com>
2017-09-06 13:03:24 -0400

    frontend development for explore or demo and permissions components

--- client/src/components/home/Home/Home.js
+++ client/src/components/home/Home/Home.js
@@ -3,15 +3,11 @@ import './Home.css';
 import Header from '../../navigation/Header';
 import Footer from '../../navigation/Footer';
 import HomeCover from './sections/HomeCover';
-// import AboutSection from './sections/AboutSection';
 import ShowcaseCollections from './sections/ShowcaseCollections';
 import FeaturesSection from './sections/FeaturesSection';
-// import TestimonialSection from './sections/TestimonialSection';
 import LifetimeData from './sections/LifetimeData';
-import WhyUsSection from './sections/WhyUsSection';
-// import BetterExperienceSection from './sections/BetterExperienceSection';
-// import PricingSection from './sections/PricingSection';
-// import CreateAccountSection from './sections/CreateAccountSection';
+import PermissionsSection from './sections/PermissionsSection';
+import ExploreOrDemo from './sections/ExploreOrDemo';
 
 class Home extends React.Component {
 	render() {
@@ -22,7 +18,8 @@ class Home extends React.Component {
 				<FeaturesSection />
 				<ShowcaseCollections />
 				<LifetimeData />
-				<WhyUsSection />
+				<PermissionsSection />
+				<ExploreOrDemo />
 				<Footer />
 			</div>
 		);
---
+++ client/src/components/home/Home/sections/PermissionsSection.js
@@ -0,0 +1,62 @@
+import React from 'react';
+import {Grid, Row, Col} from 'react-bootstrap';
+import './PermissionsSection.css';
+
+export default class PermissionsSection extends React.Component {
+	render() {
+		return (
+			<section id="Permissions" className="text-center">
+				<Grid>
+					<Row>
+						<Col md={3}>
+							<div className="permissionsHeader">
+								<h3>To share or not to share</h3>
+								<p>
+									Participate in the community of linked data and scholarship or create private collections with fine grained access controls
+								</p>
+							</div>
+						</Col>
+						<Col md={9}>
+							<Row>
+								<Col lg={6}>
+									<div className="permissionsInfoBlock">
+										<h4>User Roles</h4>
+										<p>
+											Administrators can break down Editorial teams into discrete roles and manage fine-grained permissions between roles
+										</p>
+									</div>
+								</Col>
+								<Col lg={6}>
+									<div className="permissionsInfoBlock">
+										<h4>Metadata Permissions</h4>
+										<p>
+											Field by field, choose who can see which metadata on your item records
+										</p>
+									</div>
+								</Col>
+							</Row>
+							<Row>
+								<Col lg={6}>
+									<div className="permissionsInfoBlock">
+										<h4>Linked Data Sharing</h4>
+										<p>
+											When you want to share content, you can seamless integrate collections via using shared linked data ontologies
+										</p>
+									</div>
+								</Col>
+								<Col lg={6}>
+									<div className="permissionsInfoBlock">
+										<h4>Private Collections</h4>
+										<p>
+											Share your data with just your team or in a private collection that you create
+										</p>
+									</div>
+								</Col>
+							</Row>
+						</Col>
+					</Row>
+				</Grid>
+			</section>
+		);
+	}
+}
