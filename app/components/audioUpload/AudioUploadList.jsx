f4e8ff0839e20638e1546805ca0a8b519b7e4c70
pickering.mp <pickering.mp@gmail.com>
2017-09-08 08:45:19 -0400

    update wave page, add cover section and filter section layout

--- app/components/audioUpload/AudioUploadList.jsx
+++ app/components/audioUpload/AudioUploadList.jsx
@@ -4,7 +4,11 @@ import gql from 'graphql-tag'
 import AudioUploadItem from './AudioUploadItem'
 import MuiThemeProvider from '../../../node_modules/material-ui/styles/MuiThemeProvider'
 import styles from './audioUpload.scss'
+import FlatButton from 'material-ui/FlatButton';
 import { Grid, Row, Col } from 'react-flexbox-grid-aphrodite'
+import CSSModules from 'react-css-modules'
+
+@CSSModules(styles, {allowMultiple: true})
 
 
 class AudioUploadList extends React.Component {
@@ -41,13 +45,47 @@ class AudioUploadList extends React.Component {
 			return (
 				<MuiThemeProvider>
 					<div>
-						<row>
+						<Row styleName="wave-cover-section">
+							<Col xs={12} sm={12} md={12} lg={12}>
+								<div styleName="featured-track-player-container">
+									<div styleName="featured-track-player">
+
+									</div>
+								</div>
+							</Col>
+						</Row>
+
+						<Row styleName="wave-search-filter-section">
+							<Col xs={12} sm={12} md={8} lg={8} mdOffset={2} lgOffset={2}>
+								<div>
+									<span styleName="lets-listen">Let's listen to <span styleName="filter-title">Everything</span></span>
+								</div>
+								<div styleName="filter-container">
+									<div styleName="filter-row-one">
+										<FlatButton label="Everything" />
+										<FlatButton label="Jazz" />
+										<FlatButton label="R&B" />
+										<FlatButton label="Podcasts" />
+									</div>
+									<div styleName="filter-row-two">
+										<FlatButton label="Classical" />
+										<FlatButton label="Electronic" />
+										<FlatButton label="Acoustic" />
+										<FlatButton label="Search" />
+									</div>
+								</div>
+							</Col>
+						</Row>
+
+						<Row>
 							<Col xsOffset={3} xs={6}>
 								{this.props.data.posts.map((post) =>
 									<AudioUploadItem key={post.id} post={post} />
 								)}
 							</Col>
-						</row>
+						</Row>
+
+
 					</div>
 				</MuiThemeProvider>
 			);
