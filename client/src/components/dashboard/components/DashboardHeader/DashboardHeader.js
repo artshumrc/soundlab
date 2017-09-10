930000e997bb2b7f6e0e4d2d2d32b06b6ceb7502
Luke Hollis <lukehollis@gmail.com>
2017-09-09 23:35:40 -0400

    deduplicate components with material ui components

---
+++ client/src/components/dashboard/components/DashboardHeader/DashboardHeader.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import './DashboardHeader.css';
+
+const DashboardHeader = props => (
+	<div className="dashboardHeader">
+		{/* Dashboard header content resembling homepage header content */}
+	</div>
+);
+
+export default DashboardHeader;
--- client/src/components/dashboard/components/DashboardSidebar/DashboardSidebar.js
+++ client/src/components/dashboard/components/DashboardSidebar/DashboardSidebar.js
@@ -1,4 +1,9 @@
 import React from 'react';
+import { Link } from 'react-router';
+import Drawer from 'material-ui/Drawer';
+import Menu from 'material-ui/Menu';
+import MenuItem from 'material-ui/MenuItem';
+import RaisedButton from 'material-ui/RaisedButton';
 
 import './DashboardSidebar.css';
 
@@ -6,39 +11,47 @@ class DashboardSidebar extends React.Component {
 
 	render() {
 		return (
-			<div className={sidebarClass}>
+			 <Drawer open>
 				<Link to={'/'}>
 					<h3 className="invert logo">orphe.us</h3>
 				</Link>
-				<div className="sidebarMenu">
-					<ul>
-						<ListItem
-							name="Projects"
-							url="/dashboard/projects"
-						/>
-						<ListItem
-							name="Project Editor"
-							url="/dashboard/projectEditor"
-						/>
-						<ListItem
-							name="Collection Editor"
-							url="/dashboard/collectionEditor"
-						/>
-						<ListItem
-							name="Item Editor"
-							url="/dashboard/itemEditor"
-						/>
-						<ListItem
-							name="User Profile"
-							url="/dashboard/user"
-						/>
-						<ListItem
-							name="Settings"
-							url="/dashboard/settings"
-						/>
-					</ul>
-				</div>
-			</div>
+				<Menu>
+					<MenuItem
+						primaryText="Projects"
+						url="/dashboard/projects"
+					/>
+					<MenuItem
+						primaryText="Project Editor"
+						url="/dashboard/projectEditor"
+					/>
+					<MenuItem
+						primaryText="Collections"
+						url="/dashboard/collections"
+						menuItems={[
+							<MenuItem
+								primaryText="New"
+								url="/dashboard/collections/create"
+							/>,
+							<MenuItem
+								primaryText="Edit"
+								url="/dashboard/collections/edit"
+							/>,
+						]}
+					/>
+					<MenuItem
+						primaryText="Item Editor"
+						url="/dashboard/itemEditor"
+					/>
+					<MenuItem
+						primaryText="User Profile"
+						url="/dashboard/user"
+					/>
+					<MenuItem
+						primaryText="Settings"
+						url="/dashboard/settings"
+					/>
+				</Menu>
+			</Drawer>
 		);
 	}
 }
