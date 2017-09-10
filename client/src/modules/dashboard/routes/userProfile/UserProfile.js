096ae0c0b7c53d32b1c981d7017c5a2d5a52ba31
Tyler Kirby <tyler.kirby9398@gmail.com>
2017-09-10 12:59:53 -0500

    UserProfile includes user data

--- client/src/modules/dashboard/routes/userProfile/UserProfile.js
+++ client/src/modules/dashboard/routes/userProfile/UserProfile.js
@@ -21,7 +21,7 @@ class UserProfile extends React.Component {
 				<div style={{ color: 'black' }}>
 					<h2>loading</h2>
 				</div>
-			)
+			);
 		}
 
 		return (
@@ -37,61 +37,48 @@ class UserProfile extends React.Component {
 							<Image src="/images/userProfile.jpg" responsive />
 						</Col>
 						<Col lg={6}>
-							<h3>Dr. Johnathon Doe</h3>
-							<h4>Senior Chief Trainer</h4>
+							<h3>Username: {data.userProfile.username}
+							</h3>
+							<h3>Name: 
+								{ 
+									data.userProfile.name ?  
+										data.userProfile.name 
+									: 
+										'Update your profile in the settings tab to include your bname.'
+										}
+							</h3>
 							<div className="socialIcons">
-								<a href="#twitter">
+								<a href={data.userProfile.twitter ? data.userProfile.twitter : '#twitter'}>
 									<FontAwesome name="twitter" />
 								</a>
-								<a href="#linkedin">
+								<a href={data.userProfile.linkedIn ? data.userProfile.twitter : '#twitter'}>
 									<FontAwesome name="linkedin-square" />
 								</a>
-								<a href="#facebook">
+								<a href={data.userProfile.facebook ? data.userProfile.twitter : '#twitter'}>
 									<FontAwesome name="facebook-official" />
-								</a>
-								<a href="#skype">
-									<FontAwesome name="skype" />
-								</a>
-								<a href="#pinterest">
-									<FontAwesome name="pinterest-square" />
-								</a>
-								<a href="#apple">
-									<FontAwesome name="apple" />
 								</a>
 							</div>
 							<p>
-								Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam ante ex, fermentum vel libero eget
-								interdum too semper libero. Curabitur egestas, arcu id tempor convallis ras dignissim, diam vitae ornare
-								molestie, ligula an magna imperdiet velit, volutpat tortori felis sit amet ligula. Sed in placerat
-								justo. sed, luctus mattis urna.
+								{
+									data.userProfile.bio ?
+										data.userProfile.bio
+									:
+										'Update your profile in the settings tab to include a bio.'
+								}
 							</p>
 							<ListGroup className="contactDetails">
-								<ListGroupItem>
-									<FontAwesome name="phone" />
-									<div className="details">
-										<h5>Phone:</h5>
-										<span className="value">+88 01719 45 75 93</span>
-									</div>
-								</ListGroupItem>
 								<ListGroupItem>
 									<FontAwesome name="paper-plane-o" />
 									<div className="details">
 										<h5>Email:</h5>
-										<span className="value">info@yoursite.com</span>
-									</div>
-								</ListGroupItem>
-								<ListGroupItem>
-									<FontAwesome name="mobile" />
-									<div className="details">
-										<h5>Mobile:</h5>
-										<span className="value">+88 01719 45 75 93</span>
-									</div>
-								</ListGroupItem>
-								<ListGroupItem>
-									<FontAwesome name="paper-plane-o" />
-									<div className="details">
-										<h5>Website:</h5>
-										<span className="value"><a href="http://example.com">www.johndoe.com</a></span>
+										<span className="value">
+											{
+												data.userProfile.email ?
+													data.userProfile.email
+												:
+													'Update your profile in the settings tab to include an email address.'
+											}
+										</span>
 									</div>
 								</ListGroupItem>
 							</ListGroup>
