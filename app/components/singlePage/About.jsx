f92836ab4ad003c4bde4adda67f829e2579fce90
pickering.mp <pickering.mp@gmail.com>
2017-09-13 16:44:25 -0400

    add team member section

--- app/components/singlePage/About.jsx
+++ app/components/singlePage/About.jsx
@@ -18,6 +18,25 @@ export default class About extends Component {
         backgroundSize: 'cover',
         backgroundPosition: 'center'
       }
+
+      const teamMemberImage = {
+          backgroundImage: `url("http://localhost:8888/soundlab/wp-content/uploads/2017/09/20170810_182646.jpg")`,
+          width: '100%',
+          height: '100%',
+          objectFit: 'cover',
+          backgroundSize: 'cover',
+          backgroundPosition: 'center'
+        }
+
+      const teamPickleImage = {
+          backgroundImage: `url("http://localhost:8888/soundlab/wp-content/uploads/2017/09/picklerick.jpg")`,
+          width: '100%',
+          height: '100%',
+          objectFit: 'cover',
+          backgroundSize: 'cover',
+          backgroundPosition: 'center'
+        }
+
     return (
         <div>
 
@@ -31,7 +50,31 @@ export default class About extends Component {
               <div>
                 <span styleName="about-section-title">About the Sound Lab</span>
                 <p styleName="about-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-                <span styleName="team-section-title">Team</span>
+
+                <div styleName="about-team-section">
+                  <span styleName="team-title">Team</span>
+                  <div styleName="team-member top">
+                    <div styleName="team-member-pic-container">
+                      <div style={teamMemberImage}></div>
+                    </div>
+                    <div styleName="team-member-meta">
+                      <h3 styleName="member-name">Archimedes Digital</h3>
+                      <h3 styleName="member-position">Nero d'Avola Enthusiasts</h3>
+                      <p styleName="member-about-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. </p>
+                    </div>
+                  </div>
+                  <div styleName="team-member">
+                    <div styleName="team-member-pic-container">
+                      <div style={teamMemberImage}></div>
+                    </div>
+                    <div styleName="team-member-meta">
+                      <h3 styleName="member-name">Pickle Rick</h3>
+                      <h3 styleName="member-position">Intergalatic Pickle</h3>
+                      <p styleName="member-about-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. </p>
+                    </div>
+                  </div>
+
+                </div>
               </div>
 
             </Col>
