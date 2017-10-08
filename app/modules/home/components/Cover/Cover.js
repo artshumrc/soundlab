3e9e72d21d3dd600ab409e2fd39aa2dcb0ca059a
Luke Hollis <lukehollis@gmail.com>
2017-10-08 16:35:16 -0400

    add material design icons and styles and animations for cover player

--- app/modules/home/components/Cover/Cover.js
+++ app/modules/home/components/Cover/Cover.js
@@ -1,31 +1,29 @@
 import React from 'react';
 import { Grid, Row, Col } from 'react-bootstrap';
-import CSSModules from 'react-css-modules'
+import CSSModules from 'react-css-modules';
 
 import styles from './Cover.scss';
-import FeaturedTrack from '../FeaturedTrack'
+import FeaturedTrack from '../FeaturedTrack';
 
 @CSSModules(styles, {allowMultiple: true})
 class Cover extends React.Component {
 	render() {
 		return (
-			<Grid>
-		    <Row styleName="cover-section">
-		      <Col md={6}>
-		        <div styleName="site-tag-container">
-		          <div styleName="tag-container">
-		            <span styleName="site-tag">We hear you</span>
-		          </div>
-		          <div styleName="subtitle-container">
-		            <span styleName="site-tag-subtitle">The Sound Lab at Harvard University</span>
-		          </div>
-		        </div>
-		      </Col>
-		      <Col md={6}>
-		        <FeaturedTrack />
-		      </Col>
-		    </Row>
-			</Grid>
+			<div styleName="cover">
+				<Grid>
+			    <Row>
+			      <Col md={6}>
+			        <div styleName="coverText">
+		            <h1 styleName="siteTag">We hear you</h1>
+		            <h2 styleName="siteTagSubtitle">The Sound Lab at Harvard University</h2>
+			        </div>
+			      </Col>
+			      <Col md={6}>
+			        <FeaturedTrack />
+			      </Col>
+			    </Row>
+				</Grid>
+			</div>
 		);
 	}
 }
--- app/modules/home/components/FeaturedTrack/FeaturedTrack.js
+++ app/modules/home/components/FeaturedTrack/FeaturedTrack.js
@@ -1,25 +1,84 @@
-import React, { Component, PropTypes } from 'react'
-import { Grid, Row, Col } from 'react-flexbox-grid-aphrodite'
-import CSSModules from 'react-css-modules'
+import React from 'react';
+import { Grid, Row, Col } from 'react-bootstrap';
+import CSSModules from 'react-css-modules';
+
 
 import styles from './FeaturedTrack.scss'
 
 @CSSModules(styles, {allowMultiple: true})
-
 class FeaturedTrack extends React.Component {
+	constructor(props) {
+		super(props);
+
+		this.state = {
+			status: '',
+		};
+	}
+
+	handleMouseEnter(){
+		const { status } = this.state;
+		if (status !== 'play') {
+			this.setState({
+				status: 'hover',
+			});
+		}
+	}
+
+	handleMouseLeave(){
+		const { status } = this.state;
+		if (status !== 'play') {
+			this.setState({
+				status: '',
+			});
+		}
+	}
+
+	handleClick(){
+		const { status } = this.state;
+		if (status !== 'play') {
+			this.setState({
+				status: 'play',
+			});
+		} else {
+			this.setState({
+				status: '',
+			});
+		}
+	}
 
   render() {
+		const { status } = this.state;
+
 
     return (
-
-      <div styleName="featured-track-component">
-        <div styleName="featured-track-player-container">
-
+      <div
+				styleName="featured-track-component"
+				onMouseEnter={this.handleMouseEnter.bind(this)}
+				onMouseLeave={this.handleMouseLeave.bind(this)}
+				onClick={this.handleClick.bind(this)}
+			>
+        <div
+					styleName="featured-track-player-container"
+				>
+					<div styleName="player-container">
+						<div styleName={`player-row-1 ${status === 'hover' ? 'player-row-1-hover' : ''}`} />
+						<div styleName="player-row-2" />
+						<div styleName="player-row-3" />
+						<div styleName="player-row-4" />
+						<div styleName={`player-row-5 ${status === 'hover' ? 'player-row-5-hover' : ''}`} />
+						<div styleName="play-button-container">
+							<i className="mdi mdi-play" />
+						</div>
+					</div>
         </div>
         <div styleName="featured-track-meta-container">
-          <span styleName="featured-track-title">
-            <h6>Featured Track</h6>
-          </span>
+					<div>
+	          <h6
+							styleName={`featured-track-title ${status.length ? 'featured-track-title-hover' : ''}`}
+						>
+							Featured Track
+						</h6>
+					</div>
           <span styleName="featured-track-meta-item-title">Mozart / Funk</span>
           <span styleName="featured-track-meta-item-author">Dollie Keller</span>
         </div>
