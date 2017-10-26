44cc2d7f28169e8f1eb0ec77ab4e8957de2e61c4
Luke Hollis <lukehollis@gmail.com>
2017-10-26 11:42:11 -0400

    add player functionality for one track with position/duration

--- src/app/modules/player/components/Player/Player.js
+++ src/app/modules/player/components/Player/Player.js
@@ -1,7 +1,7 @@
 import React, { Component, PropTypes } from 'react'
 import ReactDOM from 'react-dom'
 import { connect } from 'react-redux'
-import {bindActionCreators} from 'redux'
+import { bindActionCreators } from 'redux'
 import PlayArrow from 'material-ui/svg-icons/av/play-arrow'
 import Pause from 'material-ui/svg-icons/av/pause'
 import SkipPrevious from 'material-ui/svg-icons/av/skip-previous'
@@ -17,196 +17,231 @@ import _s from 'underscore.string';
 import { resumePlayer, pausePlayer, nextTrack, previousTrack } from '../../../../actions/actions'
 import FeaturedTrack from '../FeaturedTrack'
 import PlaylistList from '../../../playlist/components/PlaylistList'
+import Timer from '../Timer';
+
 import styles from './Player.scss'
+
 
 @CSSModules(styles, {allowMultiple: true})
 
 class Player extends Component {
 
-  constructor(props) {
-    super(props)
-    this.state = {
-      open:false
-    }
-  }
+	constructor(props) {
+		super(props)
+		this.state = {
+			open: false,
+			tracksWithSounds: [],
+			currentTrack: null,
+		}
+	}
 
-  componentDidMount() {
-    const { index } = this.props
-  }
+	componentDidMount() {
+		const { index } = this.props
+	}
 
-  pauseTheSound(){
-    this.props.dispatch(pausePlayer())
-    soundManager.pause(this.props.player.id)
-  }
+	componentWillReceiveProps(nextProps) {
 
-  resumeTheSound(props){
-    this.props.dispatch(resumePlayer())
-    soundManager.play(this.props.player.id)
-  }
+		// if there are tracks
+		if (
+				!this.props.tracks
+			&& nextProps.tracks
+			&& nextProps.tracks.length
+		) {
 
-  playNext(props) {
-    soundManager.destroySound(this.props.player.id)
-    this.props.dispatch(nextTrack(this.props.player.track))
-  }
-
-  playPrevious() {
-//    soundManager.destroySound(this.props.playlist[this.props.ui.currentIndex].id)
-    this.props.dispatch(previousTrack())
-  }
-
-  togglePlaylistDrawer() {
-    this.setState({
-      open:!this.state.open
-    })
-  }
+			const tracks = [{
+				id: "63",
+				post_title: "Example Sound 4",
+				post_name: "example-sound-4",
+				post_content: "Example sound from Archive.org: https://archive.org/details/Sitar96",
+				post_meta: [{
+					meta_key: "audio_file",
+					meta_value: "http://admin.soundlab.local:8888/wp-content/uploads/2017/10/Sitar96_64kb.mp3",
+					__typename: "Postmeta",
+				}],
+			}];
 
 
-  render() {
+			const tracksWithSounds = [];
+			soundManager.setup({
+				onready: () => {
+					tracks.forEach(track => {
+						const audioFile = _.findWhere(track.post_meta, { meta_key: 'audio_file' });
+						tracksWithSounds.push({
+							...track,
+							sound: soundManager.createSound({
+								id: track.post_name,
+								url: audioFile.meta_value,
+								autoPlay: false,
+								autoLoad: true,
+								whileplaying: () => {
+									//document.getElementsBystyleName(('progressBar')[0].style.width =	25 + '%')
+								},
+								onfinish: function() {
+									// document.getElementById('progressBar').style.width = '0'
+									soundManager._writeDebug(this.id + ' finished playing')
+								}
+							})
+						});
+					});
 
-    const tracks = this.props.tracks || [];
-    const playlist = []
-    const playId = this.props.player.id;
-    const playitemUrl = this.props.player.soundCloudLink;
+					console.log(tracksWithSounds);
 
-		if (!tracks || !tracks.length) {
+					this.setState({
+						tracksWithSounds,
+					});
+				},
+				ontimeout: function() {
+					// Uh-oh. No HTML5 support, SWF missing, Flash blocked or other issue
+				},
+			});
+		}
+	}
+
+	pause(){
+		this.props.dispatch(pausePlayer())
+		const track = this.state.tracksWithSounds[0];
+		soundManager.pause(track.sound.id)
+		this.setState({
+			currentTrack: null,
+		});
+	}
+
+	resume(props){
+		this.props.dispatch(resumePlayer())
+		const track = this.state.tracksWithSounds[0];
+
+		soundManager.play(track.sound.id);
+		this.setState({
+			currentTrack: track,
+		});
+	}
+
+	playNext(props) {
+		soundManager.destroySound(this.props.player.id)
+		this.props.dispatch(nextTrack(this.props.player.track))
+	}
+
+	playPrevious() {
+		// soundManager.destroySound(this.props.playlist[this.props.ui.currentIndex].id)
+		this.props.dispatch(previousTrack())
+	}
+
+	togglePlaylistDrawer() {
+		this.setState({
+			open:!this.state.open
+		})
+	}
+
+
+	render() {
+
+		// TODO: move these to redux in the future
+		let { tracksWithSounds, currentTrack } = this.state;
+
+		if (!tracksWithSounds || !tracksWithSounds.length) {
 			// TODO: just hide instead of not displaying
 			return null;
 		}
 
-		const currentTrack = tracks[0];
+		if (!currentTrack) {
+			currentTrack = tracksWithSounds[0];
+		}
 
-    const thumbnailListImage = {
-      width: '60px',
-      height: '60px',
-      objectFit: 'cover',
-      backgroundSize: 'cover',
-      backgroundRepeat: 'no-repeat',
-      backgroundPosition: 'center'
-    };
+		const thumbnailListImage = {
+			width: '60px',
+			height: '60px',
+			objectFit: 'cover',
+			backgroundSize: 'cover',
+			backgroundRepeat: 'no-repeat',
+			backgroundPosition: 'center'
+		};
 
 		if (currentTrack.thumbnail) {
-      thumbnailListImage.backgroundImage = `url("${getPostThumbnailBySize(currentTrack.thumbnail, 'thumbnail')}")`;
+			thumbnailListImage.backgroundImage = `url("${getPostThumbnailBySize(currentTrack.thumbnail, 'thumbnail')}")`;
 		} else {
-      thumbnailListImage.backgroundImage = 'url("/images/default_sound.png")';
+			thumbnailListImage.backgroundImage = 'url("/images/default_sound.png")';
 		}
 
 		const byline = _.findWhere(currentTrack.post_meta, { meta_key: 'byline' });
-		const audioFile = _.findWhere(currentTrack.post_meta, { meta_key: 'audio_file' });
 
-    const buttonStyles = {
-      height: 22,
-      width: 22,
-    };
+		const buttonStyles = {
+			height: 22,
+			width: 22,
+			cursor: "pointer",
+		};
 
-    const playButtonStyles = {
-      height: 26,
-      width: 26,
-    };
+		const playButtonStyles = {
+			height: 26,
+			width: 26,
+			cursor: "pointer",
+		};
 
-		/*
-		function playAudio(playlistId){
-        // Default playlistId to 0 if not supplied
-        playlistId = playlistId ? playlistId : 0;
-        // If SoundManager object exists, get rid of it...
-        if (playlist.nowPlaying){
-          playlist.nowPlaying.destruct();
-          // ...and reset array key if end reached
-          if(playlistId == playlist.length){
-              playlistId = 0;
-        }
-      }
-    }
-		*/
+		return (
+			<div styleName="soundlabPlayer">
+				<div styleName="soundlabPlayerContainer">
+					<div styleName="playerProgressBar" />
+					<div styleName="playerMetaContainer">
+						<div styleName="currentPlayMeta" >
+							<div styleName="playerAvatar" style={thumbnailListImage} />
+							<div styleName="playerMeta">
+								<h6 styleName="playerMetaTitle">
+									{currentTrack.post_title}
+								</h6>
+								{byline &&
+									<h6 styleName="playerMetaCreator">
+										{_s.prune(byline.meta_value, 120)}
+									</h6>
+								}
+							</div>
+						</div>
+					</div>
+					<div styleName="playerControls">
+						<div styleName="buttonWrapper">
+							<SkipPrevious
+								style={buttonStyles}
+								onClick={this.playPrevious.bind(this, tracksWithSounds)}
+							/>
 
-    soundManager.setup({
+							{this.props.ui.isPlaying ?
+								<Pause
+									style={buttonStyles}
+									onClick={this.pause.bind(this)}
+								/>
+							:
+								<PlayArrow
+									style={playButtonStyles}
+									onClick={this.resume.bind(this)}
+								/>
+							}
 
-      onready: function() {
+							<SkipNext
+								style={buttonStyles}
+								onClick={this.playNext.bind(this, tracksWithSounds)}
+							/>
+						</div>
 
-        playlist.nowPlaying = soundManager.createSound({
-          id: currentTrack.post_name,
-          url: audioFile.meta_value,
-          autoPlay: false,
-          autoLoad: true,
-          whileplaying: function() {
-          //document.getElementsBystyleName(('progressBar')[0].style.width =  25 + '%')
-        },
-         onfinish: function() {
-          // document.getElementById('progressBar').style.width = '0'
-           soundManager._writeDebug(this.id + ' finished playing')
-          }
-        })
-      },
-
-      ontimeout: function() {
-        // Uh-oh. No HTML5 support, SWF missing, Flash blocked or other issue
-      }
-    })
-
-    return (
-      <div>
-        <Row>
-          <Col xsOffset={3} xs={6}>
-            <div styleName="soundlabPlayer">
-              <div styleName="soundlabPlayerContainer">
-                <div styleName="playerMetaContainer">
-                  <div styleName="currentPlayMeta" >
-                    <div styleName="playerAvatar" style={thumbnailListImage} />
-                    <div styleName="playerMeta">
-                      <h6 styleName="playerMetaTitle">{currentTrack.post_title}</h6>
-											{byline &&
-	                      <h6 styleName="playerMetaCreator">{_s.prune(byline.meta_value, 120)}</h6>
-											}
-                    </div>
-                  </div>
-                </div>
-                <div styleName="playerControls">
-                  <div styleName="buttonWrapper">
-                    <SkipPrevious
-	                    style={buttonStyles}
-	                    onClick={this.playPrevious.bind(this, playlist)}
-                    />
-
-                    {this.props.ui.isPlaying ?
-                      <Pause
-	                      style={buttonStyles}
-	                      onClick={this.pauseTheSound.bind(this)}
-											/>
-                    :
-                      <PlayArrow
-	                      style={playButtonStyles}
-	                      onClick={this.resumeTheSound.bind(this)}
-											/>
-                    }
-
-                    <SkipNext
-	                    style={buttonStyles}
-	                    onClick={this.playNext.bind(this)}
-										/>
-                  </div>
-
-                  <div styleName="playTimeWrapper">
-	                  <span styleName="playTime">0:00 / 0:00</span>
-                  </div>
-                </div>
-              </div>
-            </div>
-          </Col>
-        </Row>
-      </div>
-    );
-  }
+						{currentTrack && currentTrack.sound &&
+							<Timer
+								track={currentTrack}
+							/>
+						}
+					</div>
+				</div>
+			</div>
+		);
+	}
 }
 
 Player.propTypes = {
-  tracks: PropTypes.array
+	tracks: PropTypes.array
 };
 
 function mapStateToProps(state){
-  return {
-    playlist: state.playlist,
-    player: state.player,
-    ui: state.ui
-  };
+	return {
+		playlist: state.playlist,
+		player: state.player,
+		ui: state.ui
+	};
 }
 
 export default connect(mapStateToProps)(Player)
