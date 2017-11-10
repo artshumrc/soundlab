eeeb2c939e2436e2370508b03b9d02c4720aa673
Luke Hollis <lukehollis@gmail.com>
2017-11-10 08:06:17 -0500

    add google tag manager information and prerender config

--- server.js
+++ server.js
@@ -4,16 +4,17 @@ import express from 'express';
 import { privateSettings } from './settings';
 import bodyParser from 'body-parser';
 import { graphqlExpress } from 'graphql-server-express';
-import { executableSchema } from './graphql/schema';
+import { executableSchema } from './schema/schema';
 
 const APP_PORT = process.env.PORT || 3000;
+const PRERENDER_TOKEN = process.env.PRERENDER_TOKEN || '';
 const app = express();
 
 app.use('/graphql', bodyParser.json(), graphqlExpress({
   schema: executableSchema
 }));
 
-app.use(require('prerender-node').set('prerenderToken', privateSettings.prerenderToken ));
+app.use(require('prerender-node').set('prerenderToken', PRERENDER_TOKEN));
 app.use(express.static('./dist'));
 app.use(express.static('./public'));
 app.get('*', function response(req, res) {
