3d909f2913f0d6486e4c76034eccf60e4e23e413
Luke Hollis <lukehollis@gmail.com>
2017-12-11 08:58:05 -0500

    add alpha in header and page title

--- src/components/navigation/Header/NavBar/NavBarHeader.js
+++ src/components/navigation/Header/NavBar/NavBarHeader.js
@@ -14,7 +14,7 @@ const NavBarHeader = ({ dispatchToggleLeftMenu, leftMenuOpen })=> (
 		/>
 		<Link to="/">
 			<h2 className="site-title">
-				orphe.us
+				orphe.us [alpha]
 			</h2>
 		</Link>
 	</div>
