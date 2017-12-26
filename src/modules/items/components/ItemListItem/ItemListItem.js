00a8107b7dc4ba6c77b688cb65fd7b6fb298fc53
Luke Hollis <lukehollis@gmail.com>
2017-12-26 14:58:58 -0500

    only display image in item list item if image is present for object

--- src/modules/items/components/ItemListItem/ItemListItem.js
+++ src/modules/items/components/ItemListItem/ItemListItem.js
@@ -1,28 +1,40 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import { Link } from 'react-router';
+import _s from 'underscore.string';
 
 import Tags from '../../../tags/components/Tags';
 
 import './ItemListItem.css';
+
 
 const ItemListItem = (props) => {
 	const itemUrl = `/items/${props.slug}`;
 
 	return (
 		<div className="itemListItem">
-			<Link to={itemUrl}>
-				<img src={props.imageUrl} alt={props.title} />
-			</Link>
+			{props.imageUrl ?
+				<Link to={itemUrl}>
+					<img src={props.imageUrl} alt={props.title} />
+				</Link>
+			: ''}
 			<Tags tags={props.tags} />
 			<Link to={itemUrl}>
 				<h3>{props.title}</h3>
 			</Link>
 			<p className="description">
-				Quid faciat laetas segetes quo sidere terram vertere Mycenas ulmisque
-				adiungere vites conveniat
+				{_s.prune(props.description, 120)}
 			</p>
 		</div>
 	);
+};
+
+ItemListItem.propTypes = {
+	imageUrl: PropTypes.string,
+	tags: PropTypes.array,
+	title: PropTypes.string,
+	slug: PropTypes.string,
+	description: PropTypes.string,
 };
 
 export default ItemListItem;
