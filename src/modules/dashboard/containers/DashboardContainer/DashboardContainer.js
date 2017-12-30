be741909997b521d090e1e9f846828c9cd7aeae0
Luke Hollis <lukehollis@gmail.com>
2017-12-30 00:15:16 -0500

    add counts for dashboard view

--- src/modules/dashboard/containers/DashboardContainer/DashboardContainer.js
+++ src/modules/dashboard/containers/DashboardContainer/DashboardContainer.js
@@ -1,10 +1,37 @@
 import React from 'react';
+import { compose } from 'react-apollo';
 
+import countsQuery from '../../graphql/queries/counts';
 import Dashboard from '../../components/Dashboard';
 
 
-const DashboardContainer = props => (
-	<Dashboard />
-);
+const DashboardContainer = props => {
+	let collectionsCount = 0;
+	let articlesCount = 0;
+	let itemsCount = 0;
+	let textsCount = 0;
 
-export default DashboardContainer;
+	if (
+			props.countsQuery
+		&& props.countsQuery.project
+	) {
+		const project = props.countsQuery.project;
+		collectionsCount = project.collectionsCount;
+		articlesCount = project.articlesCount;
+		itemsCount = project.itemsCount;
+		textsCount = project.textsCount;
+	}
+
+	return (
+		<Dashboard
+			 collectionsCount={collectionsCount}
+			 articlesCount={articlesCount}
+			 itemsCount={itemsCount}
+			 textsCount={textsCount}
+		/>
+	);
+}
+
+export default compose(
+	countsQuery,
+)(DashboardContainer);
