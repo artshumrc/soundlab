7cf4acd3ba7640c55f09bf96c79260c5ad751658
Luke Hollis <lukehollis@gmail.com>
2017-12-30 00:40:18 -0500

    fix project settings editor to initialize hostname value without orphe.us ending to hostname

--- src/modules/projects/containers/ProjectEditorContainer/ProjectEditorContainer.js
+++ src/modules/projects/containers/ProjectEditorContainer/ProjectEditorContainer.js
@@ -3,7 +3,6 @@ import { compose } from 'react-apollo';
 import autoBind from 'react-autobind';
 
 import ProjectEditor from '../../components/ProjectEditor';
-import getCurrentProjectHostname from '../../../../lib/getCurrentProjectHostname';
 import projectDetailQuery from '../../graphql/queries/detail';
 import projectUpdateMutation from '../../graphql/mutations/update';
 import projectRemoveMutation from '../../graphql/mutations/remove';
@@ -52,8 +51,13 @@ class ProjectEditorContainer extends React.Component {
 	render() {
 		let project;
 
-		if (this.props.projectQuery) {
-			project = this.props.projectQuery.project;
+		if (
+			this.props.projectQuery
+			&& this.props.projectQuery.project
+		) {
+			project = Object.assign({}, this.props.projectQuery.project);
+			// initialize hostname with orphe.us missing
+			project.hostname = project.hostname.replace('.orphe.us', '');
 		}
 
 		return (
