ac1682dd71422a4fca89a70b2fc0d34a67748e11
Luke Hollis <lukehollis@gmail.com>
2017-12-30 00:04:00 -0500

    fix item list item and project collections styles

--- src/modules/home/components/Home/Home.js
+++ src/modules/home/components/Home/Home.js
@@ -4,11 +4,11 @@ import Header from '../../../../components/navigation/Header';
 import Footer from '../../../../components/navigation/Footer';
 import HomeCover from './sections/HomeCover';
 import AboutSection from './sections/AboutSection';
-import ShowcaseCollections from './sections/ShowcaseCollections';
+// import ShowcaseCollections from './sections/ShowcaseCollections';
 import FeaturesSection from './sections/FeaturesSection';
 import LifetimeData from './sections/LifetimeData';
 import PermissionsSection from './sections/PermissionsSection';
-import ExploreOrDemo from './sections/ExploreOrDemo';
+// import ExploreOrDemo from './sections/ExploreOrDemo';
 import ReadyToStart from './sections/ReadyToStart';
 
 class Home extends React.Component {
@@ -20,7 +20,7 @@ class Home extends React.Component {
 				<AboutSection />
 				<FeaturesSection />
 
-				{/* TODO: explore collections when there are collections to explore 
+				{/* TODO: explore collections when there are collections to explorew
 					<ShowcaseCollections />
 				*/}
 
