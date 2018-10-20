dc6256d9843ca6f140c0498975172cdb6c2a1d0c
Hao Wu <haowu0802@gmail.com>
2018-10-20 12:00:51 -0700

    remove client folders;

--- src/app/animations.js
+++
@@ -1,5 +0,0 @@
-import {TweenMax} from 'gsap'
-
-export function AnimateItem(item, animation) {
-  TweenMax.to(item, animation.duration, animation.properties)
-}
--- src/app/modules/home/routes/index.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-import { IndexRoute } from 'react-router';
-import Home from '../components/Home/Home';
-
-export default (
-	<IndexRoute component={Home} />
-);
