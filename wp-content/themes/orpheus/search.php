9eca32ada24afc463e430e97b5cbe8516edbaaf1
Cole Crawford <cole_crawford@fas.harvard.edu>
2021-03-09 17:08:19 +0000

    Add orpheus theme and soundlab plugin; ignore uploads and contrib plugins

---
+++ wp-content/themes/orpheus/search.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * The template for displaying search results pages
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-hierarchy/#search-result
+ *
+ * @package WordPress
+ * @subpackage Twenty_Seventeen
+ * @since 1.0
+ * @version 1.0
+ */
+
+
+	wp_redirect( '/wp-admin' );
+	exit;
+?>
